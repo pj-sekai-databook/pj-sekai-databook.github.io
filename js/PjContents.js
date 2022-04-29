fcab2d0cc81aef29fc056fa3be5a299c0913b3d6
pj-sekai-databook <pj.sekai.databook@gmail.com>
2022-04-30 08:00:50 +0900

    動画・画像集のJSON構造の変更に対応

--- js/PjContents.js
+++ js/PjContents.js
@@ -4,7 +4,20 @@ class PjContents {
         if (this._json == null) {
             this.initJson();
         }
-        return i != null ? this._json.contents[i] : this._json.contents;
+        switch (this._json.version) {
+            case 1:
+                return i != null ? this._json.contents[i] : this._json.contents;
+            case 2:
+                if (i == null) {
+                    return this._json.contents;
+                }
+                const idx = this._json.contents.findIndex(x => x.id == i);
+                if (idx >= 0) {
+                    return this._json.contents[idx].content;
+                }
+                return undefined;
+        }
+        return undefined;
     }
     static initJson = () => {
         const is_develop = getQueryParameters().mode == "develop";
