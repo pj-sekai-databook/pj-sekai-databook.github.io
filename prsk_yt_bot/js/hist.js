a82decd3796cf5c05e2b833b235f948ff2c9c934
pj-sekai-databook <pj.sekai.databook@gmail.com>
2022-06-12 20:59:35 +0900

    再生数通知Botの更新情報を追記

--- prsk_yt_bot/js/hist.js
+++ prsk_yt_bot/js/hist.js
@@ -1,11 +1,15 @@
 const hist = [
     {
+        date: "2022/06/12",
+        html: `再生数が1,000,000回刻みの時のツイート内容を修正しました。<br />楽曲コンテスト、プロセカNEXTなどの公募採用曲も集計対象に含めるようにしました。`
+    },
+    {
         date: "2022/05/01",
         html: `プロセカYouTube再生数通知Botの集計対象動画、およびその再生数と投稿日を「プロセカDataBook」で閲覧できるようにしました。`
     },
     {
         date: "2022/04/17",
-        html: `	プロジェクトセカイ公式YouTubeチャンネルの全動画に加え、再生リスト「<a href="https://www.youtube.com/playlist?list=PLiFNg5fXiX32p4RMMDCUpjVEwXOnqyTj_" target="_blank" rel="noopener noreferrer">「プロジェクトセカイ」書き下ろし楽曲</a>」も検索対象としました。`
+        html: `プロジェクトセカイ公式YouTubeチャンネルの全動画に加え、再生リスト「<a href="https://www.youtube.com/playlist?list=PLiFNg5fXiX32p4RMMDCUpjVEwXOnqyTj_" target="_blank" rel="noopener noreferrer">「プロジェクトセカイ」書き下ろし楽曲</a>」も集計対象としました。`
     },
     {
         date: "2022/04/13",
