9be2949cdb0d137397465a349cac313827418161
pj-sekai-databook <pj.sekai.databook@gmail.com>
2022-06-25 17:59:59 +0900

    新しいキャッシュ消去処理を楽曲一覧ページで試験的に実装

---
+++ _common/js/PjLoader.js
@@ -0,0 +1,24 @@
+class PjLoader {
+    static load(depth, list = []) {
+        const stamp = new Date().getTime();
+        const base_dir = depth > 0 ? ".." + "/..".repeat(depth - 1) : ".";
+        const base_list = [
+            `${base_dir}/js/header.js`,
+            `${base_dir}/js/funcs.js`,
+            `${base_dir}/data/js/constructors.js`,
+            `${base_dir}/_common/js/class/Creator.js`,
+            `${base_dir}/data/js/classes.js`,
+            `${base_dir}/data/js/musics.js`
+        ];
+        for (const src of [...base_list, ...list]) {
+            let request = new XMLHttpRequest();
+            request.open("GET", `${src}?v=${stamp}`, false);
+            request.send(null);
+            if (request.status == 200) {
+                const script = document.createElement("script");
+                script.text = request.responseText;
+                document.head.appendChild(script);
+            }
+        }
+    }
+}
---
+++ musics/js/load.js
@@ -0,0 +1,8 @@
+PjLoader.load(1, [
+    "./js/musicsList.js",
+    "./js/sort.js",
+    "./js/filter.js",
+    "./js/checkInvalidMusic.js",
+    "./js/clearHover.js",
+    "./js/main.js"
+]);
